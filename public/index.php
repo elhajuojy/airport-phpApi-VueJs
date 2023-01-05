e5f5df66d068772d8b2c819c22b1f275e73907be
elhajuojy <souirimehdi311@gmail.com>
2023-01-05 21:14:48 +0100

    router and index files

--- public/index.php
+++ public/index.php
@@ -1,2 +1,34 @@
 <?php
 
+use Core\App;
+use Model\Client;
+
+
+session_start();
+const BASE_PATH = __DIR__.'/../';
+require_once BASE_PATH.'vendor/autoload.php';
+
+
+
+// Load database configuration from config.php file
+$config = require BASE_PATH.'config.php';
+//get the instance of the app
+$App = App::getInstance();
+//set the database configuration
+$App->setDatabase( dbConfig:   $config['database']);
+
+
+
+
+//set the routes
+$App->setRoutes([
+    '/' => 'Controller/index.php',
+    '/flights'=>'Controller/flights.php',
+    '/pilots'=>'Controller/pilots.php',
+    '/planes'=>'Controller/planes.php'
+
+],BASE_PATH);
+
+
+//run the app
+$App->run();
