09ee80ed4965172456f9f657e92412388668afe5
elhajuojy <souirimehdi311@gmail.com>
2023-01-05 20:39:37 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'airport',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];
