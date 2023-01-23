dc86d3d2af4cf0879da46c96b2485662018d853f
elhajuojy <souirimehdi311@gmail.com>
2023-01-23 22:12:00 +0100

    create and use router

---
+++ front_airports/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import router from './router'
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+
+
