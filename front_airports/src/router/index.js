26b3ff24c5c8df4bf000661733f3a4f0b4c5fc7a
elhajuojy <souirimehdi311@gmail.com>
2023-01-25 20:29:37 +0100

    commit some changes

--- front_airports/src/router/index.js
+++ front_airports/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/airports',
+      name: 'airports',
+      component: () => import('../views/AirportsView.vue')
+    },
   ]
 })
 
